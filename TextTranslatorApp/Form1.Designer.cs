14be2acd36ccf8f58a696614d994870c9893faef
vaidik <vaidik@VAIDIKVADHAB043>
2025-03-29 04:50:48 -0700

    Add project files.

---
+++ TextTranslatorApp/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace TextTranslatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxLanguage = new System.Windows.Forms.ComboBox();
+            this.buttonTranslate = new System.Windows.Forms.Button();
+            this.buttonClearAll = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBoxCapturedText = new System.Windows.Forms.ListBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBoxOriginal = new System.Windows.Forms.TextBox();
+            this.textBoxTranslated = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBoxLanguage);
+            this.panel1.Controls.Add(this.buttonTranslate);
+            this.panel1.Controls.Add(this.buttonClearAll);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 366);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 56);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBoxLanguage
+            // 
+            this.comboBoxLanguage.AccessibleName = "";
+            this.comboBoxLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLanguage.FormattingEnabled = true;
+            this.comboBoxLanguage.Location = new System.Drawing.Point(254, 18);
+            this.comboBoxLanguage.Name = "comboBoxLanguage";
+            this.comboBoxLanguage.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLanguage.TabIndex = 3;
+            this.comboBoxLanguage.SelectedIndexChanged += new System.EventHandler(this.comboBoxLanguage_SelectedIndexChanged);
+            // 
+            // buttonTranslate
+            // 
+            this.buttonTranslate.Location = new System.Drawing.Point(12, 13);
+            this.buttonTranslate.Name = "buttonTranslate";
+            this.buttonTranslate.Size = new System.Drawing.Size(106, 28);
+            this.buttonTranslate.TabIndex = 2;
+            this.buttonTranslate.Text = "Translate";
+            this.buttonTranslate.UseVisualStyleBackColor = true;
+            this.buttonTranslate.Click += new System.EventHandler(this.buttonTranslate_Click);
+            // 
+            // buttonClearAll
+            // 
+            this.buttonClearAll.Location = new System.Drawing.Point(127, 13);
+            this.buttonClearAll.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonClearAll.Name = "buttonClearAll";
+            this.buttonClearAll.Size = new System.Drawing.Size(106, 28);
+            this.buttonClearAll.TabIndex = 1;
+            this.buttonClearAll.Text = "Clear";
+            this.buttonClearAll.UseVisualStyleBackColor = true;
+            this.buttonClearAll.Click += new System.EventHandler(this.buttonClearAll_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBoxCapturedText);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(784, 366);
+            this.splitContainer1.SplitterDistance = 250;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // listBoxCapturedText
+            // 
+            this.listBoxCapturedText.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBoxCapturedText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxCapturedText.FormattingEnabled = true;
+            this.listBoxCapturedText.ItemHeight = 18;
+            this.listBoxCapturedText.Location = new System.Drawing.Point(0, 0);
+            this.listBoxCapturedText.Name = "listBoxCapturedText";
+            this.listBoxCapturedText.ScrollAlwaysVisible = true;
+            this.listBoxCapturedText.Size = new System.Drawing.Size(248, 366);
+            this.listBoxCapturedText.TabIndex = 0;
+            this.listBoxCapturedText.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBoxCapturedText_MouseClick);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBoxOriginal);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.textBoxTranslated);
+            this.splitContainer2.Size = new System.Drawing.Size(530, 366);
+            this.splitContainer2.SplitterDistance = 166;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textBoxOriginal
+            // 
+            this.textBoxOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOriginal.Location = new System.Drawing.Point(0, 0);
+            this.textBoxOriginal.Multiline = true;
+            this.textBoxOriginal.Name = "textBoxOriginal";
+            this.textBoxOriginal.Size = new System.Drawing.Size(530, 166);
+            this.textBoxOriginal.TabIndex = 0;
+            // 
+            // textBoxTranslated
+            // 
+            this.textBoxTranslated.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxTranslated.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTranslated.Location = new System.Drawing.Point(0, 0);
+            this.textBoxTranslated.Multiline = true;
+            this.textBoxTranslated.Name = "textBoxTranslated";
+            this.textBoxTranslated.Size = new System.Drawing.Size(530, 196);
+            this.textBoxTranslated.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(784, 422);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "EzTranslate";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonClearAll;
+        private System.Windows.Forms.Button buttonTranslate;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBoxCapturedText;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox textBoxOriginal;
+        private System.Windows.Forms.TextBox textBoxTranslated;
+        private System.Windows.Forms.ComboBox comboBoxLanguage;
+    }
+}
---
+++ TextTranslatorApp/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace TextTranslatorApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        private Dictionary<string, string> _translations = new Dictionary<string, string>();
+        private string _previousClipboardText = string.Empty;
+        private HttpClient _httpClient;
+        private string _logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "translator_log.txt");
+        private string _currentLanguage = "Spanish";
+        private string _currentLanguageCode = "es";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize language dropdown
+            comboBoxLanguage.Items.AddRange(new string[] {
+    "English (en)",
+    "Spanish (es)",
+    "French (fr)",
+    "German (de)",
+    "Chinese (zh)",
+    "Portuguese (pt)",
+    "Dutch (nl)",
+    "Russian (ru)",
+    "Korean (ko)",
+    "Italian (it)"
+});
+            comboBoxLanguage.SelectedIndex = 1; // Default to Spanish
+            _currentLanguage = "Spanish";
+            _currentLanguageCode = "es";
+
+            _httpClient = new HttpClient();
+            AddClipboardFormatListener(this.Handle);
+            LogMessage("Application started");
+        }
+
+        private void LogMessage(string message)
+        {
+            try
+            {
+                string logEntry = $"[{DateTime.Now}] {message}";
+                File.AppendAllText(_logPath, logEntry + Environment.NewLine);
+                Debug.WriteLine(logEntry);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error writing to log: {ex.Message}");
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                try
+                {
+                    string clipboardText = Clipboard.GetText();
+                    LogMessage($"Clipboard update detected: '{clipboardText}'");
+
+                    if (!string.IsNullOrWhiteSpace(clipboardText) && clipboardText != _previousClipboardText)
+                    {
+                        _previousClipboardText = clipboardText;
+
+                        // First add to the list
+                        if (!listBoxCapturedText.Items.Contains(clipboardText))
+                        {
+                            listBoxCapturedText.Items.Add(clipboardText);
+                            LogMessage($"Added text to list: '{clipboardText}'");
+                        }
+
+                        // Then set the text in the original textbox
+                        textBoxOriginal.Text = clipboardText;
+
+                        // Then translate
+                        TranslateTextAsync(clipboardText);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogMessage($"Error processing clipboard: {ex.Message}");
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private async void TranslateTextAsync(string text)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                this.Invoke((MethodInvoker)delegate {
+                    textBoxTranslated.Text = "Translating...";
+                });
+
+                LogMessage($"Starting translation for: '{text}' to {_currentLanguage}");
+
+                // Create a unique key for caching that includes the language
+                string cacheKey = $"{text}_{_currentLanguageCode}";
+
+                if (_translations.ContainsKey(cacheKey))
+                {
+                    LogMessage($"Found cached translation for: '{text}' to {_currentLanguageCode}");
+
+                    this.Invoke((MethodInvoker)delegate {
+                        textBoxTranslated.Text = _translations[cacheKey];
+                    });
+
+                    return;
+                }
+
+                // Update prompt to include the selected language
+                var content = new StringContent(
+                    $"{{\"model\":\"deepseek-r1:1.5b\",\"prompt\":\"Translate the following English text to {_currentLanguage}. Provide ONLY the translation with no additional text or explanation: '{text}'\",\"stream\":false}}",
+                    Encoding.UTF8,
+                    "application/json");
+
+                LogMessage("Sending request to Ollama API...");
+                var response = await _httpClient.PostAsync("http://localhost:11434/api/generate", content);
+
+                // Rest of your method remains the same
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    LogMessage($"API Response: {responseContent}");
+
+                    var translatedText = ParseOllamaResponse(responseContent);
+                    LogMessage($"Parsed translation: '{translatedText}'");
+
+                    this.Invoke((MethodInvoker)delegate {
+                        textBoxTranslated.Text = translatedText;
+                        LogMessage($"Updated textBoxTranslated.Text to: '{translatedText}'");
+                    });
+
+                    // Cache with language-specific key
+                    _translations[cacheKey] = translatedText;
+                }
+                else
+                {
+                    LogMessage($"API request failed: {response.StatusCode}");
+
+                    this.Invoke((MethodInvoker)delegate {
+                        textBoxTranslated.Text = $"Translation API request failed: {response.StatusCode}";
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"Translation error: {ex.Message}\nStack trace: {ex.StackTrace}");
+
+                this.Invoke((MethodInvoker)delegate {
+                    textBoxTranslated.Text = $"Translation error: {ex.Message}";
+                });
+            }
+            finally
+            {
+                this.Invoke((MethodInvoker)delegate {
+                    this.Cursor = Cursors.Default;
+                });
+            }
+        }
+
+
+
+        private string ParseOllamaResponse(string jsonResponse)
+        {
+            try
+            {
+                LogMessage("Parsing JSON response");
+                var responseObj = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
+                string fullResponse = responseObj.response.ToString();
+
+                // Extract only the final translation (after the </think> tag)
+                string result = fullResponse;
+
+                // Check if there's a thinking section to remove
+                if (fullResponse.Contains("</think>"))
+                {
+                    int endOfThinkingIndex = fullResponse.LastIndexOf("</think>") + 9; // 9 is the length of "</think>"
+                    if (endOfThinkingIndex < fullResponse.Length)
+                    {
+                        result = fullResponse.Substring(endOfThinkingIndex).Trim();
+                    }
+                }
+
+                LogMessage($"Extracted translation: '{result}'");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"Error parsing response: {ex.Message}\nJSON: {jsonResponse}");
+                return "Error parsing translation response";
+            }
+        }
+
+
+        private void buttonTranslate_Click(object sender, EventArgs e)
+        {
+            LogMessage("Translate button clicked");
+            string clipboardText = Clipboard.GetText();
+            if (!string.IsNullOrEmpty(clipboardText))
+            {
+                LogMessage($"Got clipboard text: '{clipboardText}'");
+                textBoxOriginal.Text = clipboardText;
+
+                // Add to list if not already present
+                if (!listBoxCapturedText.Items.Contains(clipboardText))
+                {
+                    listBoxCapturedText.Items.Add(clipboardText);
+                    LogMessage($"Added text to list: '{clipboardText}'");
+                }
+
+                // Translate using current language settings
+                TranslateTextAsync(clipboardText);
+            }
+            else
+            {
+                LogMessage("Clipboard is empty");
+                MessageBox.Show("The clipboard is empty. Please copy some text first.", "No Text to Translate", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+
+        private void buttonClearAll_Click(object sender, EventArgs e)
+        {
+            LogMessage("Clear button clicked");
+            listBoxCapturedText.Items.Clear();
+            textBoxOriginal.Text = "";
+            textBoxTranslated.Text = "";
+            _translations.Clear();
+        }
+
+        private void listBoxCapturedText_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LogMessage($"SelectedIndexChanged fired, SelectedIndex: {listBoxCapturedText.SelectedIndex}");
+            if (listBoxCapturedText.SelectedIndex != -1)
+            {
+                string selectedText = listBoxCapturedText.SelectedItem.ToString();
+                LogMessage($"List item selected: '{selectedText}'");
+
+                // Update the original text box
+                textBoxOriginal.Text = selectedText;
+
+                // Check if we have a cached translation
+                if (_translations.ContainsKey(selectedText))
+                {
+                    LogMessage("Using cached translation");
+
+                    // Ensure this update happens on the UI thread
+                    this.Invoke((MethodInvoker)delegate {
+                        textBoxTranslated.Text = _translations[selectedText];
+                        LogMessage($"Updated textBoxTranslated.Text to: '{_translations[selectedText]}'");
+                    });
+                }
+                else
+                {
+                    LogMessage("No cached translation, translating now");
+                    TranslateTextAsync(selectedText);
+                }
+            }
+        }
+
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            LogMessage("Application closing");
+            RemoveClipboardFormatListener(this.Handle);
+            _httpClient?.Dispose();
+            base.OnFormClosing(e);
+        }
+
+        private void comboBoxLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedLanguage = comboBoxLanguage.SelectedItem.ToString();
+            _currentLanguageCode = selectedLanguage.Substring(selectedLanguage.Length - 3, 2);
+            _currentLanguage = selectedLanguage.Split(' ')[0];
+
+            LogMessage($"Language changed to: {_currentLanguage} ({_currentLanguageCode})");
+
+            // Optionally retranslate current text with new language
+            if (!string.IsNullOrEmpty(textBoxOriginal.Text))
+            {
+                TranslateTextAsync(textBoxOriginal.Text);
+            }
+        }
+
+        private void listBoxCapturedText_MouseClick(object sender, MouseEventArgs e)
+        {
+            int index = listBoxCapturedText.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                string selectedText = listBoxCapturedText.Items[index].ToString();
+                LogMessage($"List item clicked: '{selectedText}'");
+
+                textBoxOriginal.Text = selectedText;
+
+                // Check if we have a cached translation for this language
+                string cacheKey = $"{selectedText}_{_currentLanguageCode}";
+
+                if (_translations.ContainsKey(cacheKey))
+                {
+                    LogMessage($"Using cached translation for {_currentLanguageCode}");
+                    this.Invoke((MethodInvoker)delegate {
+                        textBoxTranslated.Text = _translations[cacheKey];
+                    });
+                }
+                else
+                {
+                    LogMessage($"No cached translation for {_currentLanguageCode}, translating now");
+                    TranslateTextAsync(selectedText);
+                }
+            }
+        }
+
+
+
+
+        private void textBoxTranslated_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxTranslated_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxOriginal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxTranslated_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
